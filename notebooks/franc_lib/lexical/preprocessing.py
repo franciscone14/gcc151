7ea2a740cd89cb82dbfc36d0b19b5d4fae8aa357
Franciscone Luiz <franciscone@inspiron14>
2019-04-22 14:31:54 -0300

    Normalização do Corpus usando minha Lib

--- notebooks/franc_lib/lexical/preprocessing.py
+++ notebooks/franc_lib/lexical/preprocessing.py
@@ -15,45 +15,71 @@ class Preprocessing:
         if(not os.path.isdir(save_path)):
             os.makedirs(save_path)
 
-    def remove_accents(self, text):
-        return unidecode.unicode(text)
-    
-    def remove_punctuation(self, text):
-        return text.translate(str.maketrans('','', string.punctuation))
-
-    def tokenize_sentences(self, text):
-        return self.sent_tokenizer.tokenize(text)
-
-    def tokenize_words(self, text):
-        return nltk.tokenize.word_tokenize(text)
-    
-    def lemmatize(self, text):
-        return text
-
-    def stemmize(self, tokens):
-        return [self.stemmer.stem(word) for word in tokens]
-    
-    def normalization_pipeline(self, text, remove_accents=False, remove_punctuation=False, tokenize_sentences=False, tokenize_words=False, lemmatize=False, stemmize=False):
-        text = self.remove_accents(text) if remove_accents else text
-        file = self.file_name + '_no_accents.txt'
-        self.save(file_name=file, data=text)
-        text = self.remove_punctuation(text) if remove_punctuation else text
-        file = self.file_name + '_no_accents.txt'
-        self.save(file_name=file, data=text)
-        text = self.tokenize_sentences(text) if tokenize_sentences else text
-        file = self.file_name + '_no_accents.txt'
-        self.save(file_name=file, data=text)
-        text = self.tokenize_words(text) if tokenize_words else text
-        file = self.file_name + '_no_accents.txt'
-        self.save(file_name=file, data=text)
-        text = self.lemmatize(text) if lemmatize else text
-        file = self.file_name + '_no_accents.txt'
-        self.save(file_name=file, data=text)
-        text = self.stemmize(text) if stemmize else text
+    def save(self, path=None, file_name=None, data=None):
+        if not os.path.isdir(path):
+            os.makedirs(path)
         
+        with open(os.path.join(path, file_name), 'a') as file:
+            file.write('%s \n' % data)  
+
+    def remove_accents(self, text, save=False):
+        text = unidecode.unidecode(text)
+        if save:
+            path = os.path.join(self.save_path, 'accents_removed')
+            file_name = self.file_name + '_no_accents.txt'
+            self.save(path, file_name, text)
         return text
     
-    def save(self, file_name=None, data=None):
-        with open(os.path.join(self.path, file_name)) as file:
-            for d in data:
-                file.write('%s \n' % d)        
+    def remove_punctuation(self, text, save=False):
+        text = [sentence.translate(str.maketrans('','', string.punctuation)) for sentence in text]
+        if save:
+            path = os.path.join(self.save_path, 'punctuation_removed')
+            file_name = self.file_name + '_no_punct.txt'
+            self.save(path, file_name, text)
+        return text
+
+    def tokenize_sentences(self, text, save=False):
+        text = self.sent_tokenizer.tokenize(text)
+        if save:
+            path = os.path.join(self.save_path, 'tokenized_sentences')
+            file_name = self.file_name + '_tokenized_sentences.txt'
+            self.save(path, file_name, text)
+        return text
+
+    def tokenize_words(self, text, save=False):
+        tokens = [nltk.tokenize.word_tokenize(sentence) for sentence in text]
+        if save:
+            path = os.path.join(self.save_path, 'tokenized_words')
+            file_name = self.file_name + '_tokenized.txt'
+            self.save(path, file_name, tokens)
+        return tokens
+    
+    def lemmatize(self, text, save=False):
+        if save:
+            path = os.path.join(self.save_path, 'lemmatized')
+            file_name = self.file_name + '_lemmatized.txt'
+            self.save(path, file_name, text)
+        else:
+            return text
+
+    def stemmize(self, tokens, save=False):
+        stem = [self.stemmer.stem(word) for t in tokens for word in t ]
+        if save:
+            path = os.path.join(self.save_path, 'stemmized')
+            file_name = self.file_name + '_stemmized.txt'
+            self.save(path, file_name, stem)
+        return stem
+    
+    def normalization_pipeline(self, text, 
+        remove_accents=False, remove_punctuation=False, 
+        tokenize_sentences=False, tokenize_words=False, 
+        lemmatize=False, stemmize=False, save=False):
+        
+        text = self.remove_accents(text, save) if remove_accents else text
+        text = self.tokenize_sentences(text, save) if tokenize_sentences else text
+        text = self.remove_punctuation(text, save) if remove_punctuation else text
+        text = self.tokenize_words(text, save) if tokenize_words else text
+        text = self.lemmatize(text, False) if lemmatize else text
+        text = self.stemmize(text, save) if stemmize else text
+        
+        return text    
